b95ae8cebabeffbf543fa7858c8f043989e3989a
Oskar Jung <oskar@mbr-targeting.com>
2015-08-13 12:54:25 +1200

    Use ring api-defaults

--- src/iifym_tracker_api/handler.clj
+++ src/iifym_tracker_api/handler.clj
@@ -1,11 +1,11 @@
 (ns iifym-tracker-api.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes (assoc api-defaults :proxy true)))

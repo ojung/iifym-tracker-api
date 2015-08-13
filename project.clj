301dff67891b3aa2a56e99a35cdfd76116dcfe0f
Oskar Jung <oskar@mbr-targeting.com>
2015-08-13 12:52:09 +1200

    Update meta data

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject iifym-tracker-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "IFFYMTracker api"
+  :url "https://lit-temple-5563.herokuapp.com/"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler iifym-tracker-api.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}})
